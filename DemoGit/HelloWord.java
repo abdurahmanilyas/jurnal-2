9a55e20acd7e80956db75d0a6d83a257fd529be8
abdurrahman ilyas <abdurrahmanilyas39@gmail.com>
2021-09-29 11:46:52 +0700

    Helloword.java

---
+++ DemoGit/HelloWord.java
@@ -0,0 +1,8 @@
+public class HelloWord {
+    public static void main(String[] args){
+    System.out.println("Nama :abdurrahman ilyas");
+    System.out.println("NIM  :6706213014");
+    System.out.println("Kelas:4503");
+    }
+
+}
